185645791bac65d2cc4a513824c71f1a00d0b676
Ladislas de Toldi <ladislas@leka-smarttoys.com>
2013-03-19 22:02:25 +0100

    created the navbar

---
+++ navbar.php
@@ -0,0 +1,36 @@
+		<!-- Navbar
+		================================================== -->
+		<div class="navbar navbar-inverse navbar-fixed-top">
+		
+			<div class="navbar-inner">
+			
+				<div class="container">
+				
+					<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					
+					</button>
+					
+					<a class="brand" href="./index.html">HarryWishYouWell</a>
+					
+					<div class="nav-collapse pull-right collapse">
+						
+						<ul class="nav">
+						
+							<li class="active"><a href="./index.html">Home</a></li>
+							<li><a href="./a-propos.html">About</a></li>
+							<li><a href="./moti.html">FAQ</a></li>
+							<li><a href="./blog.html">Contact</a></li>			
+						
+						</ul>
+					
+					</div>	<!-- nav-collapse collapse -->
+					
+				</div>	<!-- container -->
+				
+			</div>	<!-- navbar-inner -->
+			
+		</div>	<!-- navbar navbar-inverse navbar-fixed-top -->
