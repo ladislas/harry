f9c09bfbfcc57b2096db8f85fbfc252106def68b
Ladislas de Toldi <ladislas@leka-smarttoys.com>
2013-03-20 11:28:17 +0100

    modified the ga to make it work
    Signed-off-by: Ladislas de Toldi <ladislas@leka-smarttoys.com>

--- footer.php
+++ footer.php
@@ -35,9 +35,10 @@
 ================================================== -->
 <script type="text/javascript">
 
+	<script type="text/javascript">
+
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-39424166-1']);
-  _gaq.push(['_setDomainName', 'harrywishyouwell.com']);
   _gaq.push(['_trackPageview']);
 
   (function() {
@@ -45,6 +46,8 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
+
+</script>
 
 </script>
 
